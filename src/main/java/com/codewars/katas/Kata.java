61f38202c6afd722ac908c3ff96ffe8fc4c46857
Lauren Ashton <l.v.ashton@outlook.com>
2022-02-05 11:48:56 +0000

    [Driving License kata] All except birth month complete

--- src/main/java/com/codewars/katas/Kata.java
+++ src/main/java/com/codewars/katas/Kata.java
@@ -6,63 +6,86 @@ import java.util.stream.Collectors;
 public class Kata {
     public static String driver(final String[] data) {
 
+        List<String> firstName = Collections.singletonList(data[0]);
+
+        StringBuilder middleName = new StringBuilder(data[1]);
+
         List<String> surname = Collections.singletonList(data[2]);
 
         List<String> birthDate = Collections.singletonList(data[3]);
 
         List<String> gender = Collections.singletonList(data[4]);
 
-        Map<String, String> months = new HashMap<String, String>();
+        String padding = "9";
+
+        Map<String, String> months = new HashMap<>();
         months.put("01", "Jan");
         months.put("09", "Sep");
         months.put("12", "Dec");
 
+        String arbitraryDigit = "9";
 
-        // LAST NAME (not finished)
-        String firstFiveCharsOfSurname = surname.stream()
-                .filter(name -> name.length() >= 5)
+        String computerCheck = "AA";
+
+        // LAST NAME (done)
+        StringBuilder firstFiveCharsOfSurname = new StringBuilder(surname.stream()
+                .filter(name -> name.length() <= 5)
                 .map(name -> name.toUpperCase(Locale.ROOT))
                 .map(Object::toString)
-                .collect(Collectors.joining());
+                .collect(Collectors.joining()));
+
+        while (firstFiveCharsOfSurname.length() < 5) {
+            firstFiveCharsOfSurname.append(padding);
+        }
 
         // DECADE DIGIT (done)
         String decadeDigit = birthDate.stream()
-                .map(element -> element.substring(9, 10))
+                .map(element -> element.charAt(element.length() -2))
                 .map(Objects::toString)
                 .collect(Collectors.joining());
 
-        // MONTH OF BIRTH (words) in a list
-//        List<String> birthMonth = birthDate.stream()
-//                .map(element -> element.substring(3, 6))
-//                .collect(Collectors.toList());
-
+        // BIRTH MONTH (word)
         String birthMonth = birthDate.stream()
                 .map(element -> element.substring(3, 6))
                 .map(Objects::toString)
                 .collect(Collectors.joining());
 
         // MONTH OF BIRTH (digits). need to add 50 if female
-        String birthMonthNumber = months.entrySet()
+        String birthMonthDigits = months.entrySet()
                 .stream()
                 .filter(element -> element.getValue().equals(birthMonth))
                 .map(Map.Entry::getKey)
                 .map(Objects::toString)
                 .collect(Collectors.joining());
 
-        System.out.println(birthMonthNumber);
+        //  DAY OF BIRTH
+        String birthDay = birthDate.stream()
+                .map(element -> element.substring(0,2))
+                .map(Objects::toString)
+                .collect(Collectors.joining());
 
+        // YEAR DIGIT (done)
+        String yearDigit = birthDate.stream()
+                .map(element -> element.charAt(element.length() -1))
+                .map(Objects::toString)
+                .collect(Collectors.joining());
 
-        //        Original stream
-//        return Arrays.stream(data)
-//                .skip(2)
-//                .findFirst()
-//                .map(element -> element.substring(0, 5))
-//                .stream()
-//                .map(i -> i.toUpperCase(Locale.ROOT))
-//                .collect(Collectors.joining());
-        return firstFiveCharsOfSurname + decadeDigit + birthMonthNumber;
+        // FIRST INITIAL (done)
+        String firstInitial = firstName.stream()
+                .map(element -> element.substring(0,1))
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        // MIDDLE INITIAL (done)
+
+        if(middleName.length() < 1) {
+            middleName.insert(0, padding);
+        }
+
+        String middleInitial = middleName.substring(0,1);
+
+        // RETURN STATEMENT
+
+        return firstFiveCharsOfSurname + decadeDigit + birthMonthDigits + birthDay + yearDigit + firstInitial + middleInitial + arbitraryDigit + computerCheck;
     }
 }
-
-// "John",    "James",  "Smith", "01-Jan-2000",       "M"
-// "SMITH001010JJ9AA"
